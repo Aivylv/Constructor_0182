4e7155cf7fd264800241420121d8fb40b57dfead
aivylv <lyvialarasati@gmail.com>
2024-05-14 15:43:38 +0700

    Mendefinisikan keempat konstruktor

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -12,8 +12,32 @@ public:
 	Mahasiswa(int);
 	Mahasiswa(string);
 	Mahasiswa(int iNim, string iNama);
-
+	void cetak();
 };
+
+Mahasiswa::Mahasiswa()
+{
+	nim = 0;
+	nama = "";
+}
+
+Mahasiswa::Mahasiswa(int iNim)
+{
+	nim = iNim;
+}
+
+Mahasiswa::Mahasiswa(string iNama)
+{
+	nim = 0;
+	nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama)
+{
+	nim = iNim;
+	nama = iNama;
+}
+
 int main()
 {
     
