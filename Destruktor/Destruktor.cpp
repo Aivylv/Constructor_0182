f2b7577408e35e582581ff0b422897b59d517fd5
aivylv <lyvialarasati@gmail.com>
2024-05-14 16:22:56 +0700

    Menggunakan pointer this untuk mengakses variabel member

--- Destruktor/Destruktor.cpp
+++ Destruktor/Destruktor.cpp
@@ -3,28 +3,42 @@ using namespace std;
 
 class angka {
 private:
-    int* arr;
-    int panjang;
+	int* arr;
+	int panjang;
 public:
-    angka(int);
-    ~angka();
-    void cetakData();
-    void isiData();
+	angka(int);	//constructor
+	~angka();	//destructor
+	void cetakData();
+	void isiData();
+
 };
 
-angka::angka(int i) {
-    panjang = i;
-    arr = new int[i];
-    isiData();
+//definisi member function
+angka::angka(int i) {	//constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
 }
 
-angka::~angka() {
-    cout << endl;
-    cetakData();
-    delete[]arr;
-    cout << "Alamat Array Sudah Dilepaskan" << endl;
+angka::~angka() {		//destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
-int main()
-{
-    
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = ";
+		cin >> arr[i];
+	}
+	cout << endl;
 }
+
+int main() {
+	angka belajarcpp(3);	//constructor dipanggil
+	angka* ptrBelajarcpp = new angka(5);	//constructor dipanggil
+	delete ptrBelajarcpp;	//destructor dipanggil
+
+	return 0;
+} //destructor dipanggil
